ad8b8618782bc400a385da0739a50957ca89248d
saifdhailey <ss.symb@gmail.com>
2023-01-17 17:47:37 +0530

    first commit

--- src/MyComponents/Login.js
+++ src/MyComponents/Login.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Login = (props) => {
     async function loginUser(credentials) {
-        console.log(credentials,"^^^^^^^^^^^^^^^^^^^^^^^");
+        console.log(credentials,"^^^^^^^^^");
         return fetch('https://www.mecallapi.com/api/login', {
         method: 'POST',
         headers: {
@@ -14,6 +15,9 @@ export const Login = (props) => {
     }
         
         const [loginForm, setloginForm] = useState({email:"",password:""});
+        const [iconSetHide , setIconSetHide] = useState('fa-eye-slash')
+        const [displaySaif,setDisplaySaif] = useState(true); 
+        const [passwordType , setPasswordType] = useState('password');
         const handleSubmit = async (e) => {
             e.preventDefault();
             
@@ -24,6 +28,10 @@ export const Login = (props) => {
             console.log(r1,"r1")
             // console.log(email);
         }
+
+        //setIconSetHide = () => {
+        
+       // }
         const setEmail = (event) => {
             console.log(event.target.value)
             let value = event.target.value;
@@ -37,13 +45,35 @@ export const Login = (props) => {
             setloginForm({ ...loginForm , password:value})
         }
 
+        const showHidePassword = (evt) => {
+            console.log(123)
+           
+            if (iconSetHide === 'fa-eye-slash') {
+                setIconSetHide('fa-eye')
+                setPasswordType('text')
+                setDisplaySaif(false);
+            }else {
+                setIconSetHide('fa-eye-slash')
+                setDisplaySaif(true);
+                setPasswordType('password')
+
+            }
+            
+        }
+
   return (
       <div className='auth-form-container'>
+        {
+         displaySaif ?  <p>Saif</p> : <p>Subahan</p>
+        }
       <form className='login-form' onSubmit={handleSubmit}>
           <label htmlFor="email" >Email</label>
           <input value={loginForm.email} type="email" placeholder='example@example.com' id="email" name="email" onChange={setEmail} required/>
           <label htmlFor="password" >Password</label>
-          <input value={loginForm.password} placeholder='********' id="password" name="password" onChange={setPassword} required/>
+          <div class="input-group-addon">
+        <a role={'button'} className='btn btn-primary' onClick={showHidePassword}><i className={`fa ${iconSetHide}`} aria-hidden="true"></i></a>
+      </div>
+          <input value={loginForm.password} type={passwordType} placeholder='********' id="password" name="password" onChange={setPassword} required/>
           <button type='submit'>Log In</button>
       </form>
       <button className='link-btn' onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
--- src/MyComponents/Register.js
+++ src/MyComponents/Register.js
@@ -9,7 +9,9 @@ export const Register = (props) => {
     const setfullName = (event) => {
       console.log(event.target.value)
       let value = event.target.value;
+      console.log(value)
       setregisterForm({ ...registerForm, text:value})
+      console.log(registerForm)     
   }
 
     //recaptcha function
@@ -21,20 +23,45 @@ export const Register = (props) => {
     const [registerForm, setregisterForm] = useState({name:"",email:"",password:""});
     const handleSubmit = (e) => {
         e.preventDefault();
+        console.log(registerForm)
+
+        if(registerForm.confirmPassword !== registerForm.password){
+          alert("Password does not match!");
+        }
+        
+
     }
 
+    //Confirm Password
+    const setPassword = (event) => {
+      console.log(event.target.value);
+      let value = event.target.value;
+      setregisterForm({ ...registerForm , password:value})
+    }
+    
+    const setConfirmPassword = (event) => {
+      console.log(event.target.value);
+      let value = event.target.value;
+      setregisterForm({ ...registerForm , confirmPassword:value})
+    }
+
+    const setEmail = (event) => {
+            console.log(event.target.value)
+            let value = event.target.value;
+            setregisterForm({ ...registerForm , email:value})
+    }
 
   return (
     <div className='auth-form-container'>
       <form className='register-form' onSubmit={handleSubmit}>
           <label htmlFor='name'>Full Name</label>
-          <input value={registerForm.text} name='name' id='name' placeholder='Full Name'></input>
+          <input value={registerForm.text} name='name' type="text" id='name' placeholder='Full Name' onChange={setfullName}/>
           <label htmlFor="email" >Email</label>
-          <input type="email" placeholder='example@example.com' id="email" name="email"/>
+          <input value={registerForm.email} type="email" placeholder='example@example.com' id="email" name="email" onChange={setEmail}/>
           <label htmlFor="password" >Password</label>
-          <input type="password" placeholder='********' id="password" name="password"/>
+          <input value={registerForm.password} type="password" placeholder='********' id="password" name="password" onChange={setPassword}/>
           <label htmlFor="password" >Confirm Password</label> 
-          <input type="password" placeholder='********' id="password" name="password"/>
+          <input value={registerForm.confirmPassword} type="password" placeholder='********' id="confirmPassword" name="confirmPassword" onChange={setConfirmPassword}/>
           <label for="birthday">Date of Birth:</label>
           <input type="date" id="birthday" name="birthday"/>
           <ReCAPTCHA
@@ -46,4 +73,4 @@ export const Register = (props) => {
       <button className='link-btn' onClick={() => props.onFormSwitch('login')} >Already have an account? Log In here.</button>
     </div>
   )
-}
+  }
